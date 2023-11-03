d31b33a635bb78a4529a638d82f5e394cde64fe0
Asim Ehmed <rocco.borocco95@gmail.com>
2023-11-03 22:44:36 +0400

    getted value of HTML Elements

---
+++ Age calculator/app.js
@@ -0,0 +1,11 @@
+// Getting value of Html elements
+
+const age = () => {
+    let d1 = document.getElementById("d1").value
+    let m1 = document.getElementById("m1").value
+    let y1 = document.getElementById("y1").value
+
+    // console.log(d1, m1, y1)
+
+
+}
