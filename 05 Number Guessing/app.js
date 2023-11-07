832324c3449365133fb98b4c0efb748b29637cef
Asim Ehmed <rocco.borocco95@gmail.com>
2023-11-07 14:19:00 +0400

    getting HTML Elements

---
+++ 05 Number Guessing/app.js
@@ -0,0 +1,18 @@
+// Getting HTML Elements
+
+const input = document.getElementById("input")
+const message = document.querySelector(".message")
+const guessButton = document.querySelector(".guess-btn")
+const chance = document.querySelector(".left-chance span")
+
+
+ function guess () {
+
+    let inputValue = input.value
+    // console.log(inputValue)
+
+
+
+
+
+   }
