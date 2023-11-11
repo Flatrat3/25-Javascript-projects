60047f311c12a4d14d23505938db8b8b40933427
Asim Ehmed <rocco.borocco95@gmail.com>
2023-11-11 11:39:53 +0400

    added toastr js links

---
+++ 06 Random Color Generator/app.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    addColor()
+}
+
+
+
+
+
+
+
+
+
+// Getting HTML Elements
+const button = document.querySelector(".btn")
+const randomColor = document.querySelector(".box")
