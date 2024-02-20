e8a63ae268acee8063f86d9f1a09510482b9b762
Asim Ehmed <rocco.borocco95@gmail.com>
2024-02-20 22:54:48 +0400

    Function to determine the winner of a round

--- 23 Rock Paper Scissors Game/app.js
+++ 23 Rock Paper Scissors Game/app.js
@@ -1,2 +1,28 @@
-var message = 'Hello, World!';
-console.log(message);
+// Select all buttons on the page
+var buttons = document.querySelectorAll("button");
+// console.log(buttons);
+var resultEl = document.getElementById("result");
+// Add click event listeners to each button
+buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        var result = playRound(button.id, computerPlay());
+        resultEl.textContent = result;
+    });
+});
+function computerPlay() {
+    var choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It is a!";
+    }
+    else if ((playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")) {
+        return "You win! ".concat(playerSelection, " beats ").concat(computerSelection);
+    }
+    else {
+        return "You lose! ".concat(computerSelection, " beats ").concat(playerSelection);
+    }
+}
--- 23 Rock Paper Scissors Game/app.ts
+++ 23 Rock Paper Scissors Game/app.ts
@@ -1,2 +1,33 @@
-let message: string = 'Hello, World!';
-console.log(message);
+// Select all buttons on the page
+const buttons = document.querySelectorAll<HTMLButtonElement>("button");
+// console.log(buttons);
+const resultEl = document.getElementById("result") as HTMLDivElement;
+
+
+// Add click event listeners to each button
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const result = playRound(button.id, computerPlay());
+
+    resultEl.textContent = result;
+  });
+});
+
+function computerPlay(): string {
+  const choices: string[] = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function playRound(playerSelection: string, computerSelection: string): string {
+  if (playerSelection === computerSelection) {
+    return "It is a!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+  }
+}
