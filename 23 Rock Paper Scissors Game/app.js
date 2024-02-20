f1c9d00fe76c95ec45fc77ca76571b0480a7fe9d
Asim Ehmed <rocco.borocco95@gmail.com>
2024-02-20 23:02:06 +0400

    finished

--- 23 Rock Paper Scissors Game/app.js
+++ 23 Rock Paper Scissors Game/app.js
@@ -2,6 +2,11 @@
 var buttons = document.querySelectorAll("button");
 // console.log(buttons);
 var resultEl = document.getElementById("result");
+var playerScoreEl = document.getElementById("user-score");
+var computerPlayerScore = document.getElementById("computer-score");
+// Initialize player and computer scores
+var playerScore = 0;
+var computerScore = 0;
 // Add click event listeners to each button
 buttons.forEach(function (button) {
     button.addEventListener("click", function () {
@@ -15,14 +20,19 @@ function computerPlay() {
 }
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return "It is a!";
+        return "It is tie!";
     }
     else if ((playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")) {
+        console.log(computerSelection);
+        playerScore++;
+        playerScoreEl.textContent = playerScore.toString();
         return "You win! ".concat(playerSelection, " beats ").concat(computerSelection);
     }
     else {
+        computerScore++;
+        computerPlayerScore.textContent = computerScore.toString();
         return "You lose! ".concat(computerSelection, " beats ").concat(playerSelection);
     }
 }
--- 23 Rock Paper Scissors Game/app.ts
+++ 23 Rock Paper Scissors Game/app.ts
@@ -2,13 +2,19 @@
 const buttons = document.querySelectorAll<HTMLButtonElement>("button");
 // console.log(buttons);
 const resultEl = document.getElementById("result") as HTMLDivElement;
+const playerScoreEl = document.getElementById("user-score") as HTMLSpanElement;
+const computerPlayerScore = document.getElementById(
+  "computer-score"
+) as HTMLSpanElement;
 
+// Initialize player and computer scores
+let playerScore: number = 0;
+let computerScore = 0;
 
 // Add click event listeners to each button
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const result = playRound(button.id, computerPlay());
-
     resultEl.textContent = result;
   });
 });
@@ -20,14 +26,19 @@ function computerPlay(): string {
 
 function playRound(playerSelection: string, computerSelection: string): string {
   if (playerSelection === computerSelection) {
-    return "It is a!";
+    return "It is tie!";
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
+    console.log(computerSelection);
+    playerScore++;
+    playerScoreEl.textContent = playerScore.toString();
     return `You win! ${playerSelection} beats ${computerSelection}`;
   } else {
+    computerScore++;
+    computerPlayerScore.textContent = computerScore.toString();
     return `You lose! ${computerSelection} beats ${playerSelection}`;
   }
 }
